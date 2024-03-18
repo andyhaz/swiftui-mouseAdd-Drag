04ac424387947d47f5989cd69935f64658114500
andrew <andyhaz1970@gmail.com>
2024-03-17 21:17:54 -0500

    Initial Commit

---
+++ swiftui-mouseAdd&Drag/swiftui_mouseAdd_DragApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mouseAdd_DragApp.swift
+//  swiftui-mouseAdd&Drag
+//
+//  Created by andyhaz on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mouseAdd_DragApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftui-mouseAdd&DragUITests/swiftui_mouseAdd_DragUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swiftui_mouseAdd_DragUITestsLaunchTests.swift
+//  swiftui-mouseAdd&DragUITests
+//
+//  Created by andyhaz on 3/17/24.
+//
+
+import XCTest
+
+final class swiftui_mouseAdd_DragUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
